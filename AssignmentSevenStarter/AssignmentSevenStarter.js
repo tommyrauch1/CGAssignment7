dbe576ea3786adfd906008be1493c3ddb0a936cd
tommyrauch1 <thomas.rauch@drake.edu>
2016-11-01 10:35:56 -0500

    Floor
    Made floor

--- AssignmentSevenStarter/AssignmentSevenStarter.js
+++ AssignmentSevenStarter/AssignmentSevenStarter.js
@@ -41,17 +41,29 @@ function loadPoints(points,texture) {
     //load the vertex positions and texture positions here
 
     points.push(vec4(-6.0, 0 , 10, 1));//brick corner
-    texture.push(vec2(0,0));
-    points.push(vec4(-6.0, 0, 10, 1));
-    texture.push(vec2(0,0));
-    points.push(vec4(0, 0, 10, 1));
-    texture.push(vec2(0, 0.5));
-    points.push(vec4(0, 0, 5, 1));
-    texture.push(vec2(0.5, 0.5));
+    texture.push(vec2(0,.5));
     points.push(vec4(-6.0 , 0 , 0, 1));//floor corner
     texture.push(vec2(0, 1));
-    points.push(vec4(6.0 , 0 , 0, 1));//painting corner
-    texture.push(vec2(1, 1));
+    points.push(vec4(6, 0, 0, 1));
+    texture.push(vec2(0.5, 1));
+
+
+    points.push(vec4(-6.0, 0 , 10, 1));//brick corner
+    texture.push(vec2(0,.5));
+    points.push(vec4(6, 0, 0, 1));
+    texture.push(vec2(0.5, 1));
+
+
+    points.push(vec4(6.0 , 0 , 10, 1));//floor corner
+    texture.push(vec2(.5, .5));
+
+
+    // points.push(vec4(0, 0, 5, 1));
+    // texture.push(vec2(0.5, 0.5));
+    // points.push(vec4(-6.0 , 0 , 0, 1));//floor corner
+    // texture.push(vec2(0, 1));
+    // points.push(vec4(6.0 , 0 , 0, 1));//painting corner
+    // texture.push(vec2(1, 1));
     
 
 }
